fad296ec5cdcb4ba99febcbb7a3bb65a46018d9c
iWorn Studio <niyata2206@gmail.com>
2018-12-08 19:05:20 +0700

    set version 1.002
    new upload to master

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@
  */
 function berry_setup() {
 	add_theme_support( 'custom-logo', array(
-		'width'       => 120,
-		'height'      => 120,
+		'width'       => 80, 
+		'height'      => 80, 
 		'flex-width' => true,
 		) );
 	set_post_thumbnail_size( 370, 237, TRUE );
@@ -25,16 +25,107 @@ function berry_remove_sidebar(){
 add_action( 'widgets_init', 'berry_remove_sidebar', 11 );
 
 /**
- * Enqueue scripts and styles.
+ * Anim Enqueue scripts and styles.
  */
 function berry_scripts() {
 
 	wp_dequeue_style( 'seed-style');
-
 	wp_enqueue_style( 'berry-style', get_stylesheet_uri() );
+	wp_enqueue_script( 'berry-style', get_stylesheet_directory_uri() . '/css/font-awesome.min.css');
 	wp_enqueue_script( 'berry-main', get_stylesheet_directory_uri() . '/js/main.js', array(), '2016-1', true );
+	wp_enqueue_script( 'berry-main', get_stylesheet_directory_uri() . '/js/sketchfab-viewer-1.3.1.js', array(), '2016-1', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'berry_scripts' , 20 );
 /* iworn require ACF 5 */
 define('ACF_EARLY_ACCESS','5');
+
+/**
+ * Put icon before the post title by iWorn
+ * you can create custom field for icon field by category taxonomy
+ * @see https://www.engagewp.com/how-to-add-icon-before-post-title-wordpress/ and @see https://www.wp-tweaks.com/put-image-before-the-post-title-wordpress/
+ */
+function anim_icon_before_title( $title, $id = null ) {
+
+    if(get_post_meta($id, 'icon_before_title_url', true)) {
+        $img_source = get_post_meta(get_the_ID(), 'icon_before_title_url', true);
+        $title = '<img class="icon_title" src="'. $img_source .'" />' . $title;
+   }
+
+    return $title;
+}
+add_filter( 'the_title', 'anim_icon_before_title', 10, 2 ); /* end put icon */
+
+/**
+ * Anim function Allow SVG through WordPress Media Uploader
+ */
+function cc_mime_types($mimes) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+  }
+  add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+* iWorn Add Function Admin Column Thumbnail 
+*/
+add_filter('manage_posts_columns', 'posts_columns', 10);
+add_action('manage_posts_custom_column', 'posts_custom_columns', 10, 2);
+
+function posts_columns($defaults){
+	$defaults['anim_post_thumbs'] = __('รูปปก');
+	return $defaults;
+}
+
+function posts_custom_columns($column_name, $id){
+	if($column_name === 'anim_post_thumbs'){
+		echo the_post_thumbnail( 'featured-thumbnail' );
+	}
+}/* # Thumbnail Column */
+
+/** แก้ปัญหาคุกกี้หน้า wp-login */
+setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN);
+if ( SITECOOKIEPATH != COOKIEPATH )
+	setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
+
+/**
+ * Anim Category private project_name taxonomy
+ * @package animdata 
+ */
+add_action( 'init', 'anim_private_proj_name_tax' );
+
+function anim_private_proj_name_tax() {
+    register_taxonomy(
+        'vstar-movies-2558',
+        'anim_proj_label',
+        array(
+            'label' => __( 'vstar movies 2558' ),
+            'public' => false,
+            'rewrite' => false,
+            'hierarchical' => true,
+        )
+    );
+} /** anim_private */
+
+function my_acf_result_query( $args, $field, $post )
+{
+    // eg from https://codex.wordpress.org/Class_Reference/WP_Query#Custom_Field_Parameters
+    $args['post_status'] = 'private';        
+    return $args;
+}
+
+// acf/fields/relationship/result - filter for every field
+add_filter('acf/fields/relationship/query', 'my_acf_result_query', 10, 3);
+
+/** anim post type searching */
+function anim_proj_search($template)   
+{    
+ global $wp_query;   
+ $post_type = get_query_var('post_type');   
+ if( isset($_GET['s']) && $post_type == 'project' )   
+ {
+  return locate_template('archive-search.php');  //  redirect to archive-search.php
+ }   
+ return $template;   
+}
+add_filter('template_include', 'anim_proj_search');
+/** end searching */
--- single-project.php
+++ single-project.php
@@ -10,39 +10,60 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<div class="entry-img"><?php the_post_thumbnail('full'); ?></div>
-			
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div class="container entry-img"><?php the_post_thumbnail('full'); ?></div>
+						
 			<div class="container">
 				<header class="entry-header">
-					<h1 class="entry-title" style="background-color: <?php the_field('color'); ?>;"><?php the_title(); ?></h1>
+					<h1 class="entry-title" style="background-color: <?php the_field('color'); ?>;"><?php echo "<i class='fab fa-font-awesome'></i> "; ?><?php the_title(); ?></h1>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
 					<div class="row">
 						<div class="col-sm-9">
 							<?php the_content(); ?>
+							<div class="post-type-pagination">
+								<?php $prev_post = get_previous_post();
+									if($prev_post) {
+									$prev_title = strip_tags(str_replace('"', '', $prev_post->post_title));
+									echo "\t" . '<a rel="prev" href="' . get_permalink($prev_post->ID) . '" title="' . $prev_title. '" class="single-pre">&laquo; Previous post<br /><strong>&quot;'. $prev_title . '&quot;</strong></a>' . "\n";
+									}
+
+									$next_post = get_next_post();
+									if($next_post) {
+									$next_title = strip_tags(str_replace('"', '', $next_post->post_title));
+									echo "\t" . '<a rel="next" href="' . get_permalink($next_post->ID) . '" title="' . $next_title. '" class="single-next">Next post &raquo;<br /><strong>&quot;'. $next_title . '&quot;</strong></a>' . "\n";
+									}
+								?>	</div>
+							
 						</div>
 						
 						<div class="col-sm-3">
 							<table class="table table-bordered">
 							
-							<!-- add avatar , author -->
-							<tr><th id="anim-avatar"><?php echo get_avatar(get_the_author_meta('ID'), 128 )?> </th>
-							<td><p style="text-transform:uppercase;"><strong>สร้างสรรค์โดย:</strong><?php echo get_the_author_meta('nickname') ?></p></td></tr>
-							<!-- end avatar , author -->					
+							<!-- add anim avatar , author -->
+							<tr><th id="anim-avatar"><?php echo get_avatar(get_the_author_meta('ID'), 76 )?> </th>
+								<td><span class="author-show">
+										<strong>สร้างสรรค์โดย:&nbsp;
+										<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>">
+										<?php echo get_the_author_meta('user_nicename') ?></a>
+										</strong>
+									</span>
+								</td>
+							</tr>
+							<!-- end anim avatar , or anim author -->					
 							
 								<tr><th>Year</th><td><?php the_field('year'); ?></td></tr> 	
 								
 								<?php   
 									$terms = get_the_terms( $post->ID , 'project_category' );
 									if ( $terms != null ){
-										echo '<tr><th>Division</th><td>';
+										echo '<tr><th>Style</th><td>';
 										foreach( $terms as $term ) {
 											if (($term->slug) != 'featured') {
 												echo '<a href="'. get_term_link( $term ) .'" style="color:'.get_field('color').'";>'. $term->name .'</a><br>';
@@ -52,20 +73,25 @@ get_header(); ?>
 										echo '</td></tr>';
 									} 
 								?>
-								
-								<?php   // 
+
+								<?php 
+									
 									$terms = get_the_terms( $post->ID , 'anim_project_name' );
+									$proj_count = $term->post_count;
+									$proj_count= 0;
+									$proj_count++;
+									
 									if ( $terms != null ){
-										echo '<tr><th>Projects</th><td>';
-										foreach( $terms as $term ) {
-											echo '<a href="'. get_term_link( $term ) .'" style="color:'.get_field('color').'";>'. $term->name .'</a><br>';
-										}
+										echo '<tr class="anim-proj-row"><th>Project</th>
+												<td class="style-proj-name">';
+												foreach( $terms as $term ) {
+													echo '<ul style="margin: 0 0 -0em; color:'.get_field(color).'";>
+													<li><a href="'.get_term_link( $term ) .'" style="color:'.get_field('color').'";>'. $term->name .'</a></li></ul>';
+												}
 										echo '</td></tr>';
-									} 
+									}
 								?>
-								
 								<tr><th>Color</th><td><span class="project-color" style="background-color: <?php the_field('color'); ?>;"></span></td></tr>
-								
 								</table>
 						</div>
 					</div>
@@ -81,5 +107,7 @@ get_header(); ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
+
+
 
 <?php get_footer(); ?>
